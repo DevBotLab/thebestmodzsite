00950f4f69daf26010b5191638cca307de7bf576
DevBotLab <devbotlabs@gmail.com>
2026-07-03 14:38:14 +0500

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -9,10 +7,6 @@ const nextConfig = {
         hostname: '**',
       },
     ],
-  },
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
-    return config
   },
 }
 
